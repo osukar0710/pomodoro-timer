2a181b8bb3d8d91f944b495ae6ee823c994726ed
Yoshihiko Higa <yoshi1osukar@gmail.com>
2022-11-14 03:01:59 +0900

    startbutton function add

--- pomodoroTimer/ViewController.swift
+++ pomodoroTimer/ViewController.swift
@@ -11,19 +11,21 @@ import MBCircularProgressBar
 class ViewController: UIViewController {
 
     @IBOutlet weak var progressBarView: MBCircularProgressBarView!
+    @IBOutlet weak var countText: UILabel!
+    
     var timer = Timer()
-    var startTime = 0
-    var finishTime = 10
-
+    var startProgressValue:Int = 0
+    var maxTime:Int = 1500
     
     override func viewDidLoad() {
         super.viewDidLoad()
         progressBarView.value = 0
-        // Do any additional setup after loading the view.
+        progressBarView.maxValue = CGFloat(maxTime)
+        
     }
 
     @IBAction func startButton(_ sender: UIButton) {
-        
+        timer.invalidate()
         timer = Timer.scheduledTimer(
             timeInterval: 1.0,
             target: self,
@@ -39,13 +41,30 @@ class ViewController: UIViewController {
     
     
     @IBAction func resetButton(_ sender: UIButton) {
+        timer.invalidate()
+        countText.text = "25:00"
+        startProgressValue = 0
         progressBarView.value = 0
     }
    
     @objc func updateTimer(){
-        if startTime < finishTime {
-            startTime += 1
-            progressBarView.value = CGFloat(startTime)
+        if maxTime > 1 {
+            //カウントラベル更新
+            let minutesLabel:String
+            let secondsLabel:String
+            maxTime -= 1
+            secondsLabel = String(format: "%02d", maxTime % 60)
+            
+            minutesLabel = String(format: "%02d", maxTime / 60)
+//            print(minutesLabel)
+//            print(maxTime)
+            countText.text = "\(minutesLabel):\(secondsLabel)"
+            //プログレスバー更新
+            startProgressValue += 1
+            progressBarView.value = CGFloat(startProgressValue)
+        } else {
+            timer.invalidate()
+            countText.text = "25:00"
         }
     }
     
