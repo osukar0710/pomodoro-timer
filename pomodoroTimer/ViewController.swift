8a9ccdb1c7e3b84b746325129b17d564f80e0a83
Yoshihiko Higa <yoshi1osukar@gmail.com>
2022-11-14 01:36:26 +0900

    timer add

--- pomodoroTimer/ViewController.swift
+++ pomodoroTimer/ViewController.swift
@@ -10,13 +10,27 @@ import MBCircularProgressBar
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressBarView: MBCircularProgressBarView!
+    var timer = Timer()
+    var startTime = 0
+    var finishTime = 10
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        progressBarView.value = 0
         // Do any additional setup after loading the view.
     }
 
     @IBAction func startButton(_ sender: UIButton) {
-    
+        
+        timer = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(updateTimer),
+            userInfo: nil,
+            repeats: true
+        )
     }
     
     @IBAction func stopButton(_ sender: UIButton) {
@@ -25,8 +39,15 @@ class ViewController: UIViewController {
     
     
     @IBAction func resetButton(_ sender: UIButton) {
+        progressBarView.value = 0
     }
-    
+   
+    @objc func updateTimer(){
+        if startTime < finishTime {
+            startTime += 1
+            progressBarView.value = CGFloat(startTime)
+        }
+    }
     
 }
 
